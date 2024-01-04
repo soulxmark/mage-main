3e6f8c1e4c8913ee832ed186524f362f535b31ce
soulxmark <markestimada02@gmail.com>
2024-01-04 18:21:59 +0800

    changes

--- scripts/base.js
+++ scripts/base.js
@@ -1,13 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var layers = document.querySelectorAll('.parallax-layer');
-
-    window.addEventListener('scroll', function () {
-        var yOffset = window.pageYOffset;
-
-        layers.forEach(function (layer) {
-            var speed = layer.getAttribute('data-speed');
-            var yPos = -(yOffset * speed / 100);
-            layer.style.transform = 'translate3d(0, ' + yPos + 'px, 0)';
+function scrollToSection(sectionId) {
+    const section = document.querySelector(sectionId);
+    if (section) {
+        window.scrollTo({
+            top: section.offsetTop,
+            behavior: 'smooth'
         });
-    });
-});
+    }
+}
